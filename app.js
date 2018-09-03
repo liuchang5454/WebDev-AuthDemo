77036c4cfd0de6108dfca78a8ab048b78df73794
Liu Chang <liuchang5454@gmail.com>
2018-09-03 07:06:38 +0000

    add all the 4 views, 1 model and all the starter code

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
+
+mongoose.connect("mongodb://localhost:27017/auth_demo_app", {useNewUrlParser: true});
+
+var app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//=======
+//ROUTES
+//=======
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/secret", isLoggedIn, function(req, res){
+    res.render("secret");
+});
+
+//Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+//LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "login"
+}), function(req, res){
+    
+});
+
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server has started!!");
+});
